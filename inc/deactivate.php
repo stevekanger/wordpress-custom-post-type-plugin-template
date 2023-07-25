bd477307440f04b4dcc395a68edfca5b2bf334a3
SteveKanger <hi.stevekanger@gmail.com>
2023-07-25 13:36:13 -0500

    Fixed typo on deactivation hook

--- inc/deactivate.php
+++ inc/deactivate.php
@@ -7,4 +7,4 @@ use const TemplateCustomPost\PLUGINROOT;
 function deactivate() {
     flush_rewrite_rules(); // Use when creating custom post types
 }
-register_activation_hook(PLUGINROOT, __NAMESPACE__ . '\deactivate');
+register_deactivation_hook(PLUGINROOT, __NAMESPACE__ . '\deactivate');
